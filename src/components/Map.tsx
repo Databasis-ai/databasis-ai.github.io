4cfbcdff82706cf9f797a4b86a0beea50db983dd
kyle rose <email@kylerose.io>
2023-02-22 20:18:37 -0800

    zoom level

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import Map from 'react-map-gl';
-//  [, 44.0582],
 export default function App() {
   return (
     <div>
-
       <Map
         initialViewState={{
-          latitude: 43.000000,
-          longitude: -75.000000,
-          zoom: 6
+          latitude: 33.7580624,
+          longitude: -84.3980817,
+          zoom: 12
         }}
+        scrollZoom={false}
+        dragPan={false}
         style={{width: '100%', height: 400}}
         mapStyle="mapbox://styles/anonrose/clegajk2o000a01potmh7302u"
         mapboxAccessToken={'pk.eyJ1IjoiYW5vbnJvc2UiLCJhIjoiY2xlNjloc2doMDNydjNvcHA5aDZycWdldyJ9.uLp08yXVWfvGFVGQHjRIoQ'}
