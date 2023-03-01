ca86aa09904d26c6ee15bcd95cca3b29176ca9fc
kyle rose <email@kylerose.io>
2023-02-28 19:52:17 -0800

    try

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
 import { useState, useCallback, useMemo } from 'react';
-import MapGL, { Source, Layer, Popup } from '@urbica/react-map-gl';
+import MapGL, { Source, Layer } from '@urbica/react-map-gl';
 
- const highlightLayer = {
+const highlightLayer = {
   id: 'tracts',
   type: 'fill',
   source: 'tracts',
   'source-layer': 'tracts',
+  beforeId: 'tracts-highlight',
   paint: {
     'fill-outline-color': '#484896',
     'fill-color': '#6e599f',
-    'fill-opacity': 0.75
+    'fill-opacity': 0.5
   }
 };
+
 
 export default function Mapbox() {
   const [viewport, setViewport] = useState({
@@ -21,11 +23,13 @@ export default function Mapbox() {
     zoom: 11
   });
 
-  const [tract, setTract] = useState(null) as any;
+  const [tract, setTract] = useState({ employment: 100000000 }) as any;
+
+  const [employmentFilter, setFilter] = useState(["<", ["get", "employment"], tract?.employment]) as any;
 
   const onClick = useCallback((event: any) => {
     const county = event.features && event.features[0];
-
+    setFilter(["==", ["get", "employment"], county.properties.employment]);
     setTract({
       longitude: event.lngLat.lng,
       latitude: event.lngLat.lat,
@@ -33,37 +37,38 @@ export default function Mapbox() {
     });
   }, []);
 
-  const selectedTract = tract;
-  const filter = useMemo(() => ["==", ["get", "employment"], tract?.employment], [tract?.employment]);
+  const filter = useMemo(() => employmentFilter, [tract?.employment]);
 
   return <MapGL
     style={{ width: '100%', height: '400px' }}
     mapStyle='mapbox://styles/mapbox/light-v9'
     accessToken={'pk.eyJ1IjoiYW5vbnJvc2UiLCJhIjoiY2xlNjloc2doMDNydjNvcHA5aDZycWdldyJ9.uLp08yXVWfvGFVGQHjRIoQ'}
     onViewportChange={setViewport}
-    {...viewport}
-    >
+    {...viewport}>
     <Source id='tracts' type='vector' url='mapbox://anonrose.08vc4x0b' >
-    <Layer
-      id='tracts'
-      type='fill'
-      source='tracts'
-      source-layer='tracts'
-      onClick={onClick}
-      paint={{
+      <Layer
+        id='tracts'
+        type='fill'
+        source='tracts'
+        beforeId='tracts-highlight'
+        source-layer='tracts'
+        onClick={onClick}
+        paint={{
         "fill-color": [
           "step",
           ["get", "employment"],
-          "#ffffd4", 1,
-          "#fee391", 2,
-          "#fec44f", 3,
-          "#fe9929", 4,
-          "#ec7014", 5,
-          "#cc4c02", 6,
-          "#8c2d04", 250002,
-          "#fff"
+          "#ffffcc", 1,
+          "#ffeda0", 2,
+          "#fed976", 3,
+          "#feb24c", 4,
+          "#fd8d3c", 5,
+          "#fc4e2a", 6,
+          "#e31a1c", 7,
+          "#bd0026", 8,
+          "#800026", 9,
+          '#fff'
         ],
-        'fill-opacity': 0.3,
+        'fill-opacity': 0.25,
       }}
     />
     <Layer {...highlightLayer} filter={filter} />
