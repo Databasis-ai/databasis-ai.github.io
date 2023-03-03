019550c72e6b8417be2ffc4b271c9b4ebde78dff
kyle rose <email@kylerose.io>
2023-03-02 19:37:54 -0800

    proper colors

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,15 +3,15 @@ import { useState, useCallback, useMemo } from 'react';
 import type { MapRef } from 'react-map-gl';
 import Map, { Source, Layer } from 'react-map-gl';
 const CRAZY_HIGH_EMPLOYMENT = 0;
-const DEFAULT_FILTER = ["<", ["get", "total_se_2013-2017"], CRAZY_HIGH_EMPLOYMENT];
+const DEFAULT_FILTER = ["<", ["get", "employment"], CRAZY_HIGH_EMPLOYMENT];
 import { COLOR_RANGE } from '../lib/constants';
 import bbox from '@turf/bbox';
 
 const highlightLayer = {
-  id: 'tracts-highlighted',
+  id: 'tractsclippedbystatenew-highlighted',
   type: 'fill',
-  source: 'tracts',
-  'source-layer': 'tracts',
+  source: 'tractsclippedbystatenew',
+  'source-layer': 'tractsclippedbystatenew',
   paint: {
     'fill-outline-color': '#484896',
     'fill-color': '#6e599f',
@@ -22,61 +22,63 @@ const highlightLayer = {
 export default function Mapbox() {
   const mapRef = useRef<MapRef>();
 
-  const [tract, setTract] = useState({ 'total_se_2013-2017': CRAZY_HIGH_EMPLOYMENT }) as any;
+  const [tract, setTract] = useState({ employment: CRAZY_HIGH_EMPLOYMENT }) as any;
   const [employmentFilter, setFilter] = useState(DEFAULT_FILTER) as any;
 
   const onClick = useCallback((event: any) => {
     const county = event.features && event.features[0];
+    console.log(county);
 
-    setFilter(["==", ["get", "total_se_2013-2017"], county.properties['total_se_2013-2017']]);
+    setFilter(["==", ["get", "geoid"], county.properties.geoid]);
     setTract({
       longitude: event.lngLat.lng,
       latitude: event.lngLat.lat,
-      'total_se_2013-2017': county.properties['total_se_2013-2017']
+      employment: county.properties.employment,
+      geoid: county.properties.geoid
     });
+
     const [minLng, minLat, maxLng, maxLat] = bbox(county);
-    mapRef?.current?.fitBounds(
-      [
-        [minLng, minLat],
-        [maxLng, maxLat]
-      ],
-      { padding: 40, duration: 1000 }
-    );
+      mapRef?.current?.fitBounds(
+        [
+          [minLng, minLat],
+          [maxLng, maxLat]
+        ],
+        { padding: 40, duration: 1000 }
+      );
+
   }, []);
 
-  const filter = useMemo(() => employmentFilter, [tract['total_se_2013-2017']]);
+  const filter = useMemo(() => employmentFilter, [employmentFilter]);
 
-  return <>
-    <Map
-      ref={mapRef}
-      initialViewState={{
-        latitude: 37.78,
-        longitude: -122.4,
-        zoom: 11
-      }}
-      style={{ width: '100%', height: '400px' }}
-      mapStyle='mapbox://styles/mapbox/light-v9'
-      mapboxAccessToken={'pk.eyJ1IjoiYW5vbnJvc2UiLCJhIjoiY2xlNjloc2doMDNydjNvcHA5aDZycWdldyJ9.uLp08yXVWfvGFVGQHjRIoQ'}
-      onClick={onClick}
-      interactiveLayerIds={['tracts']}>
-      <Source id='tracts' type='vector' url='mapbox://anonrose.9z1grxxg' >
-        <Layer
-          id='tracts'
-          type='fill'
-          source='tracts'
-          source-layer='tracts'
+  return <Map
+    ref={mapRef}
+    initialViewState={{
+      latitude: 37.78,
+      longitude: -122.4,
+      zoom: 11
+    }}
+    style={{ width: '100%', height: '400px' }}
+    mapStyle='mapbox://styles/mapbox/light-v9'
+    mapboxAccessToken={'pk.eyJ1IjoiYW5vbnJvc2UiLCJhIjoiY2xlNjloc2doMDNydjNvcHA5aDZycWdldyJ9.uLp08yXVWfvGFVGQHjRIoQ'}
+    onClick={onClick}
+    interactiveLayerIds={['tractsclippedbystatenew']}>
+    <Source id='tractsclippedbystatenew' type='vector' url='mapbox://anonrose.bdj30eoj' >
+      <Layer
+        id='tractsclippedbystatenew'
+        type='fill'
+          source='tractsclippedbystatenew'
+          source-layer='tractsclippedbystatenew'
           paint={{
             "fill-color": [
               "step",
-              ["get", "total_se_2013-2017"],
+              ["get", "total_est_2013-2017"],
               ...COLOR_RANGE,
-              '#fff'
+              '#000'
             ],
-            'fill-opacity': 0.25
+            'fill-opacity': 0.45
           }}
-        />
-        <Layer {...highlightLayer} filter={filter} />
-      </Source>
-    </Map>
-  </>
+      />
+      <Layer {...highlightLayer} filter={filter} />
+    </Source>
+  </Map>;
 }
