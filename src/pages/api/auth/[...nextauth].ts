18c66ed2083e40006b10754243c6866580e35ccc
kyle rose <email@kylerose.io>
2023-03-05 12:45:00 -0800

    prisma orgs

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,42 +2,46 @@ import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
-	session: {
-		// Choose how you want to save the user session.
-		// The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
-		// If you use an `adapter` however, we default it to `"database"` instead.
-		// You can still force a JWT session by explicitly defining `"jwt"`.
-		// When using `"database"`, the session cookie will only contain a `sessionToken` value,
-		// which is used to look up the session in the database.
-		strategy: "database",
-
-		// Seconds - How long until an idle session expires and is no longer valid.
-		maxAge: 30 * 24 * 60 * 60, // 30 days
-
-		// Seconds - Throttle how frequently to write to database to extend a session.
-		// Use it to limit write operations. Set to 0 to always update the database.
-		// Note: This option is ignored if using JSON Web Tokens
-		updateAge: 24 * 60 * 60, // 24 hours
-
+	adapter: PrismaAdapter(prisma),
+	pages: {
+		signIn: '/enter'
 	},
 	callbacks: {
-		session({ session, user }) {
-			if (session.user) {
-				session.user.id = user.id;
-			}
-			return session;
+		session(params) {
+			return params.session;
 		},
+		async redirect({ url }) {
+			return '/reports'
+		}
 	},
-	adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: '390510178312-ic9vpej0mrge0pkad7oa93fctvdgv85h.apps.googleusercontent.com',
 			clientSecret: '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',
-		}),
+		})
 	],
+	events: {
+		async createUser(message) {
+			const user = message.user;
+			// insert organization, assign user to unknown organization
+			await prisma.organization.create({
+				data: {
+					name: 'unknown',
+					users: {
+						connect: {
+							id: user.id
+						}
+					}
+				}
+			});
+			return;
+		}
+	}
 };
-// GOOGLE_CLIENT_ID=390510178312-ic9vpej0mrge0pkad7oa93fctvdgv85h.apps.googleusercontent.com
-// GOOGLE_CLIENT_SECRET=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
 export default NextAuth(authOptions);
+
+
+
